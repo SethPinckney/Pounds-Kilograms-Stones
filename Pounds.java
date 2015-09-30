e2bdbbf634ba0c88a6d55e7fef3083b48af037e2
Seth Pinckney <sethpinckney@Seths-MacBook-Pro.local>
2015-09-30 12:53:28 -0400

    Tried to do this in the command line, but didn't work for me. He's my notation anyway.

---
+++ Pounds.java
@@ -0,0 +1,29 @@
+public class Pounds {
+	
+	public static void main(String args []){
+	
+		
+		for (int lb = 0; lb <= 235; lb++) {
+			double kg = lb * 2.2;
+			System.out.println(lb+"\t"+kg);
+					
+					
+		//how to create a for loop to continue converting pounds to kilograms to stones 	
+			
+		}
+		
+		{
+			System.out.println("");
+				System.out.println("");
+					System.out.println("");
+		
+		}
+		for (int kg = 1; kg <= 517.0;kg++){
+			double st = kg * 0.157473;
+			System.out.println(kg+"\t"+st);
+			
+			
+		}
+		
+	}
+}
